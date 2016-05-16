adb8f85675b867ce88540d02eb4791750dede15c
German <german.goldenstein@gmail.com>
2016-05-16 17:21:54 -0300

    Remove/Change some files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 var VideoCall = {
+  
+  /* 1) */
   requestMediaStream: function(event) {
     navigator.getUserMedia(
       {video: true, audio: false},
@@ -7,12 +9,14 @@ var VideoCall = {
     );
   },
 
+  /* 2) onSuccess */
   onMediaStream: function(stream) {
     VideoCall.localVideo = document.getElementById('myVideo');
     VideoCall.localStream = stream;
-    VideoCall.localVideo.srcObject = stream;
+    VideoCall.localVideo.srcObject = stream; //<video>
   },
 
+  /* 3) Error callback */
   noMediaStream: function(error) {
     console.log('Something went wrong...', error);
   }
