e297efda27ada6d46840dedc450d997c900699a9
German <german.goldenstein@gmail.com>
2016-05-18 19:46:02 -0300

    Let's start with some code

---
+++ js/app.js
@@ -0,0 +1,20 @@
+VideoCall = {
+
+	//Call getUserMedia API
+	requestMediaStream: function() {
+		
+	},
+
+	//getUserMedia success callback
+	onSuccessCallBack: function (stream){
+		
+	},
+
+	//getUserMedia success callback
+	onErrorCallBack: function (err) {
+		
+	}
+
+}
+
+VideoCall.requestMediaStream();
