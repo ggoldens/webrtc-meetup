66d76b47f6cc2f4db1f94d5fa996b0caa74449ad
German <german.goldenstein@gmail.com>
2016-05-18 19:46:56 -0300

    Implement localStream

--- js/app.js
+++ js/app.js
@@ -2,17 +2,21 @@ VideoCall = {
 
 	//Call getUserMedia API
 	requestMediaStream: function() {
-		
+		var constraints = {video:true, audio:false};
+		navigator.getUserMedia(constraints, this.onSuccessCallBack, this.onErrorCallBack);
 	},
 
 	//getUserMedia success callback
 	onSuccessCallBack: function (stream){
-		
+		this.localVideo = document.getElementById('myVideo');
+		this.localStream = stream;
+		this.localVideo.srcObject = stream;
+
 	},
 
 	//getUserMedia success callback
 	onErrorCallBack: function (err) {
-		
+		console.log('navigator error:', err);
 	}
 
 }
