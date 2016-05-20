3619acd61ac2c8374576acb9d3a27af936be8336
German <german.goldenstein@gmail.com>
2016-05-20 12:10:03 -0300

    Remove comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 VideoCall = {
 
-  //Call getUserMedia API
   requestMediaStream: function() {
     
   },
 
-  //getUserMedia success callback
   onSuccessCallback: function (stream){
     
   },
 
-  //getUserMedia success callback
   onErrorCallback: function (err) {
     
   }
