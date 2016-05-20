f95faff031479633ecaace60a26b790e8be0b545
German <german.goldenstein@gmail.com>
2016-05-20 09:41:03 -0300

    Remove tabs

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
 VideoCall = {
 
-	//Call getUserMedia API
-	requestMediaStream: function() {
-		var constraints = {video:true, audio:false};
-		navigator.getUserMedia(constraints, this.onSuccessCallback, this.onErrorCallback);
-	},
+  requestMediaStream: function() {
+    var constraints = {video:true, audio:false};
+      navigator.getUserMedia(constraints, this.onSuccessCallback, this.onErrorCallback);
+    },
 
-	//getUserMedia success callback
-	onSuccessCallback: function (stream){
-		this.localVideo = document.getElementById('myVideo');
-		this.localStream = stream;
-		this.localVideo.srcObject = stream;
+  onSuccessCallback: function (stream){
+    this.localVideo = document.getElementById('myVideo');
+    this.localStream = stream;
+    this.localVideo.srcObject = stream;
+  },
 
-	},
-
-	//getUserMedia on error callback
-	onErrorCallback: function (err) {
-		console.log('navigator error:', err);
-	}
-
+  onErrorCallback: function (err) {
+    console.log('navigator error:', err);
+  }
 }
 
 VideoCall.requestMediaStream();
