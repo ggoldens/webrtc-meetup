6f6b5c72cfbb5447accf43c69a09d946948a4074
German <german.goldenstein@gmail.com>
2016-05-20 09:39:40 -0300

    Remove comments

--- js/app.js
+++ js/app.js
@@ -1,31 +1,24 @@
 VideoCall = {
 
-  //Call getUserMedia API
   requestMediaStream: function() {
     var constraints = {video:true, audio:false};
     navigator.getUserMedia(constraints, this.onSuccessCallback, this.onErrorCallback);
   },
 
-  //getUserMedia success callback
   onSuccessCallback: function (stream){
     this.localVideo = document.getElementById('myVideo');
     this.localStream = stream;
     this.localVideo.srcObject = stream;
   },
 
-  //getUserMedia on error callback
   onErrorCallback: function (err) {
     console.log('navigator error:', err);
   },
 
-  //start a call 
   startCall: function () {
-    // 1) create a peer connection object
-    // 2) create and send the offer
     
   },
-
-  //hang up a call 
+  
   hangUpCall: function () {
     
   },
