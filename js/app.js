02a51c5a05fac2c005b9b9c5faff393519825d46
German <german.goldenstein@gmail.com>
2016-05-20 13:27:02 -0300

    Add basics lines to step0

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
+var STUN = {
+	urls: ['stun:stun.l.google.com:19302'] 
+};
+
+var logError = function (err) { console.log(err) }
+
 VideoCall = {
-
+	socket: io('https://webrtc-meetup-io.herokuapp.com'),
   requestMediaStream: function() {
-    
+		 // The fun starts here!
   },
 
-  onSuccessCallback: function (stream){
-    
+  startCall: function () {
+
   },
 
-  onErrorCallback: function (err) {
-    
-  }
+  hangUp: function () {
+
+  },
 
 }
+
+var call = document.getElementById('call');
+var hangup = document.getElementById('hangup');
+call.onclick = VideoCall.startCall;
+hangup.onclick = VideoCall.hangUp;
+
 
 VideoCall.requestMediaStream();
