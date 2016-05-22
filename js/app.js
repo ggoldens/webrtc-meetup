0a999a8bb65e88f8e7e27a8e88ba443da44f0cf6
German <german.goldenstein@gmail.com>
2016-05-22 14:11:16 -0300

    Changes

--- js/app.js
+++ js/app.js
@@ -1,26 +1,18 @@
 var STUN = {
-	urls: ['stun:stun.l.google.com:19302'] 
+	urls:'stun:stun.l.google.com:19302'
 };
-
-var logError = function (err) { console.log(err) }
 
 VideoCall = {
 	socket: io('https://webrtc-meetup-io.herokuapp.com'),
-
-  requestMediaStream: function() {
+	requestMediaStream: function() {
 		 // The fun starts here!
   },
 
   startCall: function () { },
 
-  hangUp: function () { },
-
 }
 
 var call = document.getElementById('call');
-var hangup = document.getElementById('hangup');
 call.onclick = VideoCall.startCall;
-hangup.onclick = VideoCall.hangUp;
-
 
 VideoCall.requestMediaStream();
