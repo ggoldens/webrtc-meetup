00468ff5e5a74047ef53ae1018a479bc0b60b2fe
Daniel Phillips <phiro56@gmail.com>
2016-05-27 15:38:33 -0600

    video demo styles

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+  $( " .video-box " ).click(function() {
+    $( " .video-box " ).toggleClass( "top-vid" );
+  });
+
+  $( " .call-btn " ).click(function() {
+    $( " .video-wrap " ).toggleClass( "twoVideo" );
+  });
+
+twoVideo
+
+});
